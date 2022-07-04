d6684fcc3141f0a30446adaa7c6e641b00e0a569
Leo Elias <108422919+Le-Thon@users.noreply.github.com>
2022-07-04 12:46:49 +0100

    Add files via upload

---
+++ gggne/plugins/المطور.py
@@ -0,0 +1,59 @@
+import random
+import re
+import time
+from platform import python_version
+
+from telethon import version, Button
+from telethon.errors.rpcerrorlist import (
+    MediaEmptyError,
+    WebpageCurlFailedError,
+    WebpageMediaEmptyError,
+)
+from telethon.events import CallbackQuery
+
+from gggne import StartTime, gggne, gggneversion
+
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers.functions import ialivetext, check_data_base_heal_th, get_readable_time
+from ..helpers.utils import reply_id
+from ..sql_helper.globals import gvarstatus
+from . import mention
+
+plugin_category = "utils"
+
+@gggne.ar_cmd(
+    pattern="المطور$",
+    command=("المطور", plugin_category),
+    info={
+        "header": "لأظهار مطورين السورس",
+        "usage": [
+            "{tr}المطور",
+        ],
+    },
+)
+async def amireallyalive(event):
+    "A kind of showing bot details"
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
+    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
+    CAT_IMG = gvarstatus("ALIVE_PIC")
+    if CAT_IMG:
+        CAT = [x for x in CAT_IMG.split()]
+        A_IMG = list(CAT)
+        PIC = random.choice(A_IMG)
+        cat_caption = f"- Lethon UserBot Devs\n"
+        cat_caption += f"✛━━━━━L E T H O N━━━━━✛\n"
+        cat_caption += f"- Developed By  : @GGGNE\n"
+        cat_caption += f"- Hope you enjoy it if you find a problem don't forget to contact me .\n"
+        cat_caption += f"✛━━━━━L E T H O N━━━━━✛\n"
+        await event.client.send_file(
+            event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
+        )
+
+@gggne.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+async def on_plug_in_callback_query_handler(event):
+    statstext = await catalive(StartTime)
+    await event.answer(statstext, cache_time=0, alert=True)
